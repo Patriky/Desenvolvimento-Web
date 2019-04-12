25c546213d6291eeec880a59a7e2b5dcb40df506
Patriky <patriky.galvao@gmail.com>
2019-04-11 23:48:42 -0300

    Upload img in movies

--- TicketForFun/app/models/movie.rb
+++ TicketForFun/app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+	has_attached_file :avatar, styles: { medium: "400x200>", thumb: "100x100>" }, default_url: "/assets/default.png"
+	validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end

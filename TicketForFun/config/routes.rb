5f3b0dba86748f89b7cbf7caa499703051478325
Lincoln <linkskill@gmail.com>
2019-05-16 12:46:27 -0300

    Creating Collaborator and fixing Carousel.

--- TicketForFun/config/routes.rb
+++ TicketForFun/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :collaborators
+
   resources :welcome
 
   root 'welcome#index'
--- TicketForFun/db/schema.rb
+++ TicketForFun/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_190237) do
+ActiveRecord::Schema.define(version: 2019_05_16_145928) do
+
+  create_table "collaborators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
